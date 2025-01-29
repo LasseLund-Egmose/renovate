e5cdc4af802881418fc5329b7738d50d8fc24761
Lasse Lund-Egmose <llde@ramboll.dk>
2025-01-29 12:53:00 +0100

    Test

--- renovate-config.js
+++ renovate-config.js
@@ -1,24 +1,7 @@
 module.exports = {
-    branchPrefix: 'test-renovate/',
-    username: 'renovate-release',
+    branchPrefix: 'renovate/',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    onboarding: false,
     platform: 'github',
     forkProcessing: 'enabled',
-    repositories: ['LasseLund-Egmose/app-service-python-test'],
-    packageRules: [
-      {
-        description: 'lockFileMaintenance',
-        matchUpdateTypes: [
-          'pin',
-          'digest',
-          'patch',
-          'minor',
-          'major',
-          'lockFileMaintenance',
-        ],
-        dependencyDashboardApproval: false,
-        minimumReleaseAge: '0 days',
-      },
-    ],
+    repositories: ['LasseLund-Egmose/app-service-python-test']
 };
